05525def4adc09771859d2a0a1e8ead22830db20
Emre Badem <emrebadem_@hotmail.com>
2021-12-15 01:29:14 +0300

    Repo and subscriber store.

---
+++ src/MailSubscriberStore.ts
@@ -0,0 +1,15 @@
+// Needs a proper database.
+class MailSubscriberStore {
+  private subscribers = new Map<string, { email: string; subscribed_at: Date }[]>();
+  addSubscriber(repoName: string, email: string, cron: unknown) {
+    const subscriberToPush = { email, subscribed_at: new Date() };
+    const currentSubscribers = this.subscribers.get(repoName);
+    if (!currentSubscribers) this.subscribers.set(repoName, [subscriberToPush]);
+    else currentSubscribers.push(subscriberToPush);
+  }
+  removeSubscriber(repoName: string, email: string) {
+    const currentSubscribers = this.subscribers.get(repoName);
+    if (currentSubscribers) throw new Error('Subscription not found');
+  }
+}
+export default MailSubscriberStore;
