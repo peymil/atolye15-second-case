987d1d187de33bd28e67f118ef0527159638be84
Emre Badem <emrebadem_@hotmail.com>
2021-12-15 10:48:22 +0300

    Repostore property names changed.

--- src/RepoStore.ts
+++ src/RepoStore.ts
@@ -1,16 +1,16 @@
 // Needs a proper database.
 class RepoStore {
   private repos = new Map<string, { sha: string; updatedPackages: string }>();
-  getInspectedRepo(repoName: string, newSHA: string) {
+  getRepo(repoName: string, newSHA: string) {
     const repo = this.repos.get(repoName);
     if (!repo) return undefined;
     if (repo.sha !== newSHA) return undefined;
     return repo.updatedPackages;
   }
-  deleteInspectedRepo(repoName: string) {
+  deleteRepo(repoName: string) {
     this.repos.delete(repoName);
   }
-  addInspectedRepo(repoName: string, sha: string, updatedPackages: string) {
+  addRepo(repoName: string, sha: string, updatedPackages: string) {
     this.repos.set(repoName, { sha, updatedPackages });
   }
 }
